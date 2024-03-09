53d4782c310dae2a413fe4600f08b03d3181d699
azzamt11 <abdullahazzamt11@gmail.com>
2024-03-09 17:07:03 +0700

    bug-fixing

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -75,7 +75,7 @@ class _ProfilePageState extends State<ProfilePage> {
           const Align(
             alignment: Alignment.bottomCenter,
             child: SizedBox(
-              height: 160,
+              height: 180,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
